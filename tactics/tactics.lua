6018dd1af9035d26853da8a464afad79e76b8160
Fenyvesolgyi Zeteny <gaplaster3600@gmail.com>
2026-01-01 12:36:27 +0100

    yap

--- tactics/tactics.lua
+++ tactics/tactics.lua
@@ -1,65 +1,112 @@
-role = ""
-defaultSpeed = 60
-defaultKick = 100
+role = "" -- The player's role
+defaultSpeed = 60	-- sets a default speed
+fastSpeed = 75 -- sets a fast speed
+defaultKick = 100	-- sets a default kick speed
+targetpos = Vec2:New(0, 0)	-- the current target's position
 
-function GetDistanceAndAngle(first, second)
-	local heightdiff = math.abs(first.y-second.y)
-	local widthdiff = math.abs(first.x-second.x)
-	local hypotenuse = math.sqrt(widthdiff*widthdiff + heightdiff*heightdiff)
-	local direction = math.asin(heightdiff / hypotenuse)
-	if (first.x < second.x) then
-		direction = math.pi - direction
+
+function GetDistanceAndAngle(a, b)	-- a function with two distance arguments
+	local lengthdiff = math.abs(a.y-b.y)	-- sets the length difference
+	local widthdiff = math.abs(a.x-b.x)		-- sets the width difference
+	for i=0,3 do	-- searches for all the opponents
+		pos = API.Locate.Opp(i)
+		print("The opponents position: ", pos.x, pos.y)
+		opp_pos = {pos.x, pos.y}
+		
+		if i ~= player then		-- if the player is not us
+			pos = API.Locate.Teammate(i) -- locate all the teammates
+			print("The teammates position: ", pos.x, pos.y)
+			tm8_pos = {pos.x, pos.y}
+		end
 	end
-	if (first.y < second.y) then
-		direction = direction + 2*(math.pi - direction)
+
+	-- dist = Vec2:New(myself).dist(Vec2:New(ball))
+	print(dist(myself, ball))	--print the distance between ourselves and the ball
+
+
+	local hypotenuse = math.sqrt(widthdiff*widthdiff + lengthdiff*lengthdiff)	-- uses the Pythegorian theorem
+	local direction = math.atan(lengthdiff, widthdiff)	-- calculates the direction with an asin function
+--[[ 	if (a.x < b.x) then		-- if the ball is on my left
+		direction = math.pi - direction		-- corrects the direction
 	end
+	if (a.y < b.y) then		if the ball is behind me
+		direction = direction + 2*(math.pi - direction)		-- corrects the direction
+	end ]]
 	return hypotenuse, direction
 end
 
-function OnInit()
-	if player == 0 then
-		role = "keeper"
-	else
-		role = "striker"
-	end
-	print(role)
+function dist(a, b)		-- a distance calculating function
+	local lengthdiff = math.abs(a.y-b.y)	-- the difference in length is the subtraction's absolute value
+	local widthdiff = math.abs(a.x-b.x)		-- the difference in width is the subtraction's absolute value
+	return math.sqrt(widthdiff*widthdiff + lengthdiff*lengthdiff)	-- uses the direction formula
 end
 
-function OnUpdate(dt)
-	ball = API.Locate.Ball()
-	myself = API.Locate.Teammate(player)
-	distance, direction = GetDistanceAndAngle(ball, myself)
-	if (role == "keeper") then
-		if (distance < 30.1) then
-			choice = math.random(1, 3)
-			mate = API.Locate.Teammate(choice)
-			mateDistance, mateDirection = GetDistanceAndAngle(mate, ball)
-			API.Robot.Kick(mateDirection, defaultKick)
-		else
-			if direction < math.pi then
-				direction = (math.pi / 2)
-			else
-				direction = 3*(math.pi / 2)
-			end
-			API.Robot.Move(direction, defaultSpeed)
-		end
-	elseif (distance < 30.1) then
-		if math.random(0, 4) < 4 then
-			target = API.Locate.OppGoal()
-			
-		else
-			choice = math.random(1, 3)
-			if choice == player then
-				choice = choice + 1
-			end
-			if choice > 3 then
-				choice = 1
-			end
-			target = API.Locate.Teammate(choice)
-		end
-		targetDistance, targetDirection = GetDistanceAndAngle(target, ball)
-		API.Robot.Kick(targetDirection, defaultKick)
+function OnInit()
+	if player == 0 then		-- if our id is 0
+		role = "keeper"		-- we are the goalkeepers
 	else
-		API.Robot.Move(direction, defaultSpeed)
+		role = "striker"		-- we are the strikers
 	end
+	print(role)		-- print the player's role
+end
+
+bm_dis_ext = bm_distance + 10
+om_dis_ext = om_distance + 10
+tm_dis_ext = tm_distance + 10
+ob_dis_ext = ob_distance + 10
+tb_dis_ext = tb_distance + 10
+to_dis_ext = to_distance + 10
+circle_radius = 200
+
+function OnUpdate(dt)
+	ball = API.Locate.Ball()		-- initializing the balL's position
+	myself = API.Locate.Teammate(player)	-- defines the position of ourselves
+
+	bm_distance, bm_direction = GetDistanceAndAngle(ball, myself)
+	om_distance, om_direction = GetDistanceAndAngle(opp_pos, myself)
+	tm_distance, tm_direction = GetDistanceAndAngle(tm8_pos, myself)
+	ob_distance, ob_direction = GetDistanceAndAngle(opp_pos, ball)
+	tb_distance, tb_direction = GetDistanceAndAngle(tm8_pos, ball)
+	to_distance, to_direction = GetDistanceAndAngle(tm8_pos, opp_pos)
+	def_mes = true
+	strikerpos = {0, 0}
+	defenderpos = {0, 0}
+	keeperpos = {0, 0}
+	defendid = ""
+
+	if role == "striker" && bm_distance < 50 && ob_distance > bm_dis_ext then
+		API.Robot.Move(bm_direction, fastSpeed)
+		API.Robot.Kick(tm_direction, 80)
+	end
+
+	elseif role == "striker" && ob_distance <= bm_dis_ext then 
+		if def_mes then
+			if role == "defender" && 250 <= strikerpos.y <= 350 then
+				if defendid = "left" then
+					API.Robot.Move(strikerpos.x - 75, defaultSpeed)
+					API.Robot.Move(strikerpos.y - 75, defaultSpeed)
+				end
+				if defendid = "left" then
+					API.Robot.Move(strikerpos.x - 75, defaultSpeed)
+					API.Robot.Move(strikerpos.y + 75, defaultSpeed)
+				end
+			elseif role == "defender" && 0 < strikerpos < 250 then
+				API.Robot.Move(strikerpos.x - 75, defaultSpeed)
+				API.Robot.Move(strikerpos.y + 75, defaultSpeed)
+			end
+			elseif role == "defender" && 350 < strikerpos < 600 then
+				API.Robot.Move(strikerpos.x - 75, defaultSpeed)
+				API.Robot.Move(strikerpos.y - 75, defaultSpeed)
+			end
+			end
+		end
+	else:
+		API.Robot.Move(bm_direction, defaultSpeed)
+	end
+
+	elseif role == "keeper" && bm_distance <= 400 then 
+		API.Robot.Move(bm_direction, defaultSpeed)
+		API.Robot.Kick(tm_direction, 120)
+	end
+	
 end
